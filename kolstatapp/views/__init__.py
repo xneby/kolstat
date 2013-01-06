20f21612c156ec659b64491bb9afbc32c2b45f5e
Maciej Filip Kacprzak <paramaciej@gmail.com>
2013-01-06 16:30:47 +0100

    dodanie widoku pojedynczej serii

--- kolstatapp/views/__init__.py
+++ kolstatapp/views/__init__.py
@@ -5,4 +5,4 @@ from train import train, train_date
 from journey import journey_add, journey_add_step2, journey_add_step3, journey_add_finitialize, journey_add_end
 from user import profile, journeys
 from admin import train_list, train_delete, train_add
-from unit import units_classes
+from unit import units_classes, units_class
